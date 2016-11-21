6ae9dcdde6f3e59310b237757ade623e7b6b9749
weapon <weaponhe@126.com>
2016-11-21 11:57:40 +0800

    enhance tooltip

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
     <app-bar :fixed="true">
-      <tooltip  slot="left" content="left tooltip"><icon name="account"></icon></tooltip>
+      <tooltip  slot="left" content="left tooltip" direction="bottom"><icon name="account"></icon></tooltip>
       <tooltip  slot="right"><icon name="add"></icon></tooltip>
     </app-bar>
-    <div class="test"></div>
+    <div class="test">
+      <div class="test-button">
+        <tooltip direction="top" theme="dark" content="top tooltip">
+          <tooltip direction="bottom" theme="light" content="bottom tooltip">
+            <tooltip direction="left" theme="light" content="left tooltip">
+              <tooltip direction="right" theme="light" content="right tooltip">
+                <div class="cube"></div>
+              </tooltip>
+            </tooltip>
+          </tooltip>
+        </tooltip>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,8 +27,17 @@
 </script>
 
 <style>
+
   .test{
     height: 1000px;
-    background-color: red;
+    text-align: center;
+  }
+  .test-button{
+    padding-top: 200px;
+  }
+  .cube{
+    width:100px;
+    height: 100px;
+    background-color: green;
   }
 </style>
--- src/components/tooltip/src/tooltip.vue
+++ src/components/tooltip/src/tooltip.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="tooltip">
-       <!--@mouseenter="handleShowPopper"-->
-       <!--@mouseleave="handleClosePopper">-->
+  <div class="tooltip"
+       @mouseenter="handleShowPopper"
+       @mouseleave="handleClosePopper">
 
     <div class="tooltip-rel" ref="reference">
       <slot></slot>
@@ -9,9 +9,10 @@
 
     <div class="tooltip-popper"
          v-show="showPopper"
-         ref="popper">
+         ref="popper"
+         :class="['direction-' + direction,'theme-' + theme]">
       <slot name="content">
-        <div v-text="content"></div>
+        <div class="tooltip-popper-content" v-text="content"></div>
       </slot>
     </div>
   </div>
@@ -30,7 +31,15 @@
         type: Number,
         default: 0
       },
-      content: String
+      content: String,
+      direction:String,
+      theme:{
+        type:String,
+        default:'dark',
+        validator: function (value) {
+          return value === 'light' ? value : 'dark'
+        }
+      }
     },
     methods: {
       handleShowPopper(){
@@ -49,17 +58,143 @@
 <style scoped lang="less" rel="stylesheet/less">
   .tooltip {
     display: inline-block;
+    position: relative;
   }
 
   .tooltip-rel {
     display: inline-block;
-    position: relative;
   }
 
-  .tooltip-popper {
+  .tooltip-popper{
     position: absolute;
-    left: 100%;
-    top:0;
+    padding: 7px;
+    border-radius: 3px;
   }
+
+  .theme-dark{
+    background-color: rgba(0,0,0,0.8);
+    color:#FFF;
+  }
+
+  .theme-light{
+    background-color: #FFF;
+    color:#555;
+    border:1px solid  rgba(0,0,0,0.8);
+  }
+
+ .theme-light::before{
+    position: absolute;
+    display: block;
+    width: 0;
+    height: 0;
+    border-color: transparent;
+    border-style: solid;
+    content: " ";
+    border-width: 7px;
+ }
+
+
+  .tooltip-popper-content{
+    white-space: nowrap;
+  }
+
+  .tooltip-popper::after {
+    position: absolute;
+    display: block;
+    width: 0;
+    height: 0;
+    border-color: transparent;
+    border-style: solid;
+    content: " ";
+    border-width: 8px;
+  }
+
+  .direction-top{
+    bottom:100%;
+    transform:translateX(-50%);
+    left: 50%;
+    margin-bottom: 10px;
+  }
+
+  .direction-bottom{
+    top:100%;
+    transform:translateX(-50%);
+    left: 50%;
+    margin-top: 10px;
+  }
+  
+  .direction-left{
+    right:100%;
+    transform:translateY(-50%);
+    top: 50% ;
+    margin-right: 10px;
+  }
+  
+  .direction-right{
+    left:100%;
+    transform:translateY(-50%);
+    top: 50%;
+    margin-left: 10px;
+  }
+
+  .direction-top::after{
+    border-top-color: rgba(0,0,0,0.8);
+    top: 100%;
+    left: 50%;
+    transform:translateX(-50%);
+  }
+
+ .theme-light.direction-top::before{
+    border-top-color: #FFF;
+    top: 100%;
+    left: 50%;
+    transform:translateX(-50%);
+    z-index: 1;
+ }
+
+  .direction-bottom::after{
+    border-bottom-color: rgba(0,0,0,0.8);
+    bottom: 100%;
+    left: 50%;
+    transform:translateX(-50%);
+  }
+
+   .theme-light.direction-bottom::before{
+    border-bottom-color: #FFF;
+    bottom: 100%;
+    left: 50%;
+    transform:translateX(-50%);
+    z-index: 1;
+   }
+  
+  .direction-left::after{
+    border-left-color: rgba(0,0,0,0.8);
+    left: 100%;
+    top: 50%;
+    transform:translateY(-50%);
+  }
+
+  .theme-light.direction-left::before{
+    border-left-color: #FFF;
+    left: 100%;
+    top: 50%;
+    transform:translateY(-50%);
+    z-index: 1;
+   }
+  
+  .direction-right::after{
+    border-right-color: rgba(0,0,0,0.8);
+    right: 100%;
+    top: 50%;
+    transform:translateY(-50%);
+  }
+
+  .theme-light.direction-right::before{
+    border-right-color: #FFF;
+    right: 100%;
+    top: 50%;
+    transform:translateY(-50%);
+    z-index: 1;
+   }
 
 </style>
