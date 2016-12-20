88153c74823e3027ce715e1e462d8312d8633027
weaponhe <weaponhe@126.com>
2016-12-21 01:35:20 +0800

    breadcrumb

--- src/components/main-view/Breadcrumb.vue
+++ src/components/main-view/Breadcrumb.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="breadcrumb">
       <span v-for="(todo,index) in itemList">
-        {{index===0?'':'>'}}
-        <router-link :to="todo.path">
+        {{index===0?'':' / '}}
+        <router-link :to="todo.path" class="router-link">
          {{todo.title}}
         </router-link>
       </span>
@@ -10,45 +10,34 @@
 </template>
 
 <script>
-    export default{
-    name:'breadcrumb',
-      computed:{
-        itemList(){
-          let titleList = this.$route.path.split('/'),
-          path = titleList.slice(0,2).join('/'),
+  export default{
+    name: 'breadcrumb',
+    computed: {
+      itemList(){
+        let titleList = this.$route.path.split('/'),
+          path = titleList.slice(0, 2).join('/'),
           ret = []
-          titleList.slice(2).forEach(title=>{
-            path += '/' + title
-            ret.push(this.$store.state.todo[path])
-          })
-          return ret
-        }
-       }
+        titleList.slice(2).forEach(title => {
+          path += '/' + title
+          ret.push(this.$store.state.todo[path])
+        })
+        return ret
+      }
     }
-
-
-
-
-
-
-
-
-
+  }
 </script>
 
-<style>
-  .breadcrumb{
+<style scoped lang="less" rel="stylesheet/less">
+
+  .breadcrumb {
+    color: #ccc;
+    font-size: 14px;
+    .router-link {
+      color: #aaa;
+      &:hover{
+        color: #4fc08d
+      }
+    }
+
   }
-
-
-
-
-
-
-
-
-
-
-
-
 </style>
